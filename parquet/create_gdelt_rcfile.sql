3b3bce43504c2faaa0f1d0390b66b0b4d60d6d97
Travis L Pinney <travis.pinney@gmail.com>
2013-09-28 10:28:32 -0400

    Add RCFILE table creation

--- parquet/create_gdelt_rcfile.sql
+++ parquet/create_gdelt_rcfile.sql
@@ -3,6 +3,7 @@ SET hive.exec.compress.output=true;
 SET mapred.max.split.size=256000000;
 SET mapred.output.compression.type=BLOCK;
 SET mapred.output.compression.codec=org.apache.hadoop.io.compress.SnappyCodec;
+CREATE TABLE gdelt_rcfiletest LIKE gdelt STORED AS RCFILE;
 INSERT OVERWRITE gdelt_rcfiletest SELECT * FROM gdelt;
 
 
